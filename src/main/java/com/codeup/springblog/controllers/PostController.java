040aac7ea66907d31e4dc0b3882bc6419cce6137
Oscar Castro <oscar.a.castro818@gmail.com>
2022-05-16 13:45:08 -0500

    feat: completed posts exercise, created html views for single post and all posts

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,38 +1,66 @@
 package com.codeup.springblog.controllers;
 
 
+import com.codeup.springblog.model.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
 
-    @GetMapping("/posts")
-    @ResponseBody
-    public String posts() {
-        return "posts index page";
+    public List<Post> generatePost () {
+        Post secondPost = new Post(2, "Apple Post", "All apple products");
+        Post thirdPost = new Post(3, "Tesla Post", "All Tesla products");
+        List<Post> allPosts = new ArrayList<>();
+        allPosts.add(secondPost);
+        allPosts.add(thirdPost);
+         return allPosts;
     }
+
+
+    @GetMapping("/posts")
+    public String posts(Model model) {
+
+       List<Post> allPosts = generatePost();
+
+        model.addAttribute("posts", allPosts);
+
+        return "posts/index";
+    }
+
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String postById(@PathVariable long id) {
-        return "view an individual post (" + id + ")";
+    public String postById(@PathVariable long id, Model model) {
+
+        List<Post> allPosts = generatePost();
+            Post post = null;
+        for (int i = 0 ; i < allPosts.size(); i++) {
+            if (allPosts.get(i).getId() == id){
+                post = allPosts.get(i);
+            }
+            model.addAttribute("post", post);
+        }
+
+        return "posts/show";
     }
 
-    @GetMapping("/posts/create")
-    @ResponseBody
-    public String createPostForm() {
-        return "view the form for creating a post";
-    }
 
-    @PostMapping("/posts/create")
-    @ResponseBody
-    public String createPost() {
-        return "create a new post";
-    }
+//    @GetMapping("/posts/create")
+//    @ResponseBody
+//    public String createPostForm() {
+//        return "view the form for creating a post";
+//    }
+//
+//    @PostMapping("/posts/create")
+//    @ResponseBody
+//    public String createPost() {
+//        return "create a new post";
+//    }
 
 
 }
---
+++ src/main/java/com/codeup/springblog/model/Post.java
@@ -0,0 +1,63 @@
+package com.codeup.springblog.model;
+
+import java.util.List;
+
+public class Post {
+
+    private long id;
+    private String title;
+    private String description;
+//    private List<Post> posts;
+
+    public Post(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public Post(long id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public Post() {
+    }
+
+//    public Post(List<Post> posts) {
+//        this.posts = posts;
+//    }
+//
+//    public List<Post> getPosts() {
+//        return posts;
+//    }
+//
+//    public void setPosts(List<Post> posts) {
+//        this.posts = posts;
+//    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+}
