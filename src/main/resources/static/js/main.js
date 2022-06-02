c0e1560130da09b6d7f019ab66b55aafad90c919
Oscar Castro <oscar.a.castro818@gmail.com>
2022-06-02 11:18:24 -0500

    feat: Dane fixed issues with javascript files

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -7,3 +7,34 @@ $(".edit-post").on("click", function () {
     let postId = $(this).attr("data-id")
     window.location = "/posts/" + postId + "/edit"
 });
+
+
+
+
+// client.picker().open();
+
+
+// th:inline="javascript";
+
+
+//testing it gets passed through
+
+const client = filestack.init(API_KEY);
+const options = {
+    fromSources: ["local_file_system","instagram","facebook"],
+    onUploadDone:
+        function (res){
+            $("#image3").val(res.filesUploaded[0].url);
+            console.log("url + " + res.filesUploaded[0].url);
+        }
+}
+
+
+$(".upload-picture").on("click", function () {
+// alert("testing")
+    client.picker(options).open()
+});
+
+// document.querySelector('.upload-picture').addEventListener("click", function () {
+//     alert("Your api key is: " + apiKey);
+// });
